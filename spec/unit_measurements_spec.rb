bef17303827a0f9d81114a4b67b7a606ea258b0e
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-09-17 21:19:59 +0530

    Bump version to `1.6.0`

--- spec/unit_measurements_spec.rb
+++ spec/unit_measurements_spec.rb
@@ -4,6 +4,6 @@
 
 RSpec.describe UnitMeasurements do
   it "has a valid version number" do
-    expect(UnitMeasurements::VERSION).to eq("1.5.0")
+    expect(UnitMeasurements::VERSION).to eq("1.6.0")
   end
 end
