5163082c5ba1e43baf17f48799e5cc08a8c85ae4
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-09-20 11:23:13 +0530

    Added unit group for `temperature` units

--- lib/unit_measurements/unit_groups/all.rb
+++ lib/unit_measurements/unit_groups/all.rb
@@ -2,7 +2,10 @@
 # -*- frozen_string_literal: true -*-
 # -*- warn_indent: true -*-
 
+# Fundamental SI units
+
 require_relative "amount_of_substance"
 require_relative "length"
+require_relative "temperature"
 require_relative "time"
 require_relative "weight"
---
+++ spec/unit_measurements/unit_groups/temperature_spec.rb
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
+# -*- warn_indent: true -*-
+
+# spec/unit_measurements/unit_groups/temperature_spec.rb
+
+RSpec.describe UnitMeasurements::Temperature do
+  describe "K" do
+    subject { described_class.new(2, :K) }
+
+    it "converts to °R" do
+      expect(subject.convert_to(:°R).quantity).to eq(3.6)
+    end
+  end
+
+  describe "°R" do
+    subject { described_class.new(2, :°R) }
+
+    it "converts to K" do
+      expect(subject.convert_to(:K).quantity).to eq(1.11111111111111)
+    end
+  end
+end
