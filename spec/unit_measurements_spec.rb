74cef56acd330a6a09f659225447494b31e39788
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-09-28 19:03:41 +0530

    Bump version to `3.4.0`

--- spec/unit_measurements_spec.rb
+++ spec/unit_measurements_spec.rb
@@ -4,6 +4,6 @@
 
 RSpec.describe UnitMeasurements do
   it "has a valid version number" do
-    expect(UnitMeasurements::VERSION).to eq("3.3.0")
+    expect(UnitMeasurements::VERSION).to eq("3.4.0")
   end
 end
