58e2beb9757019cf56f066fd8b98aed8c78eaf7b
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-10-22 22:40:05 +0530

    Increased test coverage

--- spec/unit_measurements_spec.rb
+++ spec/unit_measurements_spec.rb
@@ -3,6 +3,46 @@
 # -*- warn_indent: true -*-
 
 RSpec.describe UnitMeasurements do
+  describe ".build" do
+    it "creates a new unit group based on provided block" do
+      unit_group = UnitMeasurements.build do
+        primitive "m"
+
+        system :metric do
+          si_unit "m", aliases: ["meter", "metre", "meters", "metres"]
+        end
+
+        system :imperial do
+          unit "in", value: "25.4 mm", aliases: ['"', "inch", "inches"]
+        end
+
+        cache "length.json"
+      end
+
+      expect(unit_group).to be_a(Class)
+      expect(unit_group.unit_group).to be_a(UnitMeasurements::UnitGroup)
+    end
+
+    it "returns a subclass of Measurement with defined units and conversions" do
+      unit_group = UnitMeasurements.build do
+        primitive "m"
+
+        system :metric do
+          si_unit "m", aliases: ["meter", "metre", "meters", "metres"]
+        end
+
+        system :imperial do
+          unit "in", value: "25.4 mm", aliases: ['"', "inch", "inches"]
+        end
+
+        cache "length.json"
+      end
+
+      expect(unit_group.ancestors).to include(UnitMeasurements::Measurement)
+      expect(unit_group.unit_group.units.length).to eq(26)
+    end
+  end
+
   describe ".configuration" do
     it "returns a Configuration instance" do
       expect(described_class.configuration).to be_an_instance_of(UnitMeasurements::Configuration)
