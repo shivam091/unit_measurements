3c13dfc7bd41d1b8195aeb54251a7c280e3c7c3d
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-10-26 21:52:33 +0530

    Added new units `N`, `s`, `drt`, and `lbt` in weight

--- lib/unit_measurements/unit_groups/weight.rb
+++ lib/unit_measurements/unit_groups/weight.rb
@@ -10,27 +10,28 @@ UnitMeasurements::Weight = UnitMeasurements.build do
 
     unit "q", value: "100 kg", aliases: ["quintal", "quintals"]
     unit "t", value: "1000 kg", aliases: ["tonne", "tonnes", "metric tonne", "metric tonnes"]
+    unit "N", value: "0.10197162129779283 kg", aliases: ["newton", "newtons"]
+
     unit "ct", value: "200 mg", aliases: ["carat", "carats"]
     unit "Da", value: "1.660538921e-27 kg", aliases: ["dalton", "daltons"]
   end
 
   system :imperial do
-    unit "lb", value: [Rational(45_359_237, 1e+8), "kg"], aliases: ["lbs", "lbm", "pound-mass", "pound", "pounds", "#"]
+    unit "s", value: "20 gr", aliases: ["scruple", "scruples"]
+
+    unit "lb", value: "0.45359237 kg", aliases: ["lbs", "lbm", "pound-mass", "pound", "pounds", "#"]
     unit "st", value: "14 lb", aliases: ["stone", "stones"]
-    unit "oz", value: [Rational(1, 16), "lb"], aliases: ["ounce", "ounces"]
-  end
+    unit "oz", value: "1/16 lb", aliases: ["ounce", "ounces"]
+    unit "dr", value: "1/16 oz", aliases: ["dram", "drams"]
+    unit "gr", value: "1/7000 lb", aliases: ["grain", "grains"]
 
-  system :troy_weight do
-    unit "gr", value: [Rational(1, 7000), "lb"], aliases: ["grain", "grains"]
+    unit "drt", value: "60 gr", aliases: ["troy dram", "troy drams"]
     unit "ozt", value: "480 gr", aliases: ["troy ounce", "troy ounces"]
-    unit "dwt", value: [Rational(1, 20), "ozt"], aliases: ["pennyweight", "pennyweights"]
-  end
-
-  system :avoirdupois do
-    unit "dr", value: [Rational(1, 16), "oz"], aliases: ["dram", "drams"]
+    unit "lbt", value: "5760 gr", aliases: ["troy pound", "troy pounds"]
+    unit "dwt", value: "1/20 ozt", aliases: ["pwt", "pennyweight", "pennyweights"]
   end
 
   system :foot_pound_second do
-    unit "slug", value: "32.1740 lb", aliases: ["slugs"]
+    unit "slug", value: "32.17404856 lb", aliases: ["slugs"]
   end
 end
