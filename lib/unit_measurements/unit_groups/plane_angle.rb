29fde5beaf3af379b1512c1770cb450e6bef93c4
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-11-04 19:03:00 +0530

    Added new units `tr`, `cyc`, `sign`, `octant`, `sextant`, and `quadrant` in `plane angle`

--- lib/unit_measurements/unit_groups/plane_angle.rb
+++ lib/unit_measurements/unit_groups/plane_angle.rb
@@ -8,15 +8,23 @@ UnitMeasurements::PlaneAngle = UnitMeasurements.build do
   system :metric do
     si_unit "rad", aliases: ["radian", "radians"]
 
-    unit "deg", value: [(Math::PI / 180), "rad"], aliases: ["°", "degree", "degrees"] # (π / 180) rad
-    unit "gon", value: [(Math::PI / 200), "rad"], aliases: ["ᵍ", "grad", "gradian", "gradians"] # (π / 200) rad
-    unit "arcmin", value: [Rational(1, 60), "deg"], aliases: ["′", "amin", "arcminute", "arcminutes"] # ((π / 180) / 60) rad
-    unit "arcsec", value: [Rational(1, 60), "arcmin"], aliases: ["″", "asec", "arcsecond", "arcseconds"] # ((π / 180) / 3600) rad
+    unit "deg", value: [(Math::PI / 180), "rad"], aliases: ["°", "degree", "degrees", "degree of arc", "degrees of arc", "arcdegree", "arcdegrees"]
+    unit "gon", value: [(Math::PI / 200), "rad"], aliases: ["ᵍ", "grad", "gradian", "gradians"]
+    unit "arcmin", value: [Rational(1, 60), "deg"], aliases: ["′", "amin", "arcminute", "arcminutes"]
+    unit "arcsec", value: [Rational(1, 60), "arcmin"], aliases: ["″", "asec", "arcsecond", "arcseconds"]
   end
 
-  unit "cir", value: [360, "deg"], aliases: ["circle", "circles"] # (2 * π) rad
-  unit "mil", value: [Rational(1, 6400), "cir"], aliases: ["mils"] # ((2 * π) / 6400) rad
-  unit "rev", value: [1, "cir"], aliases: ["revolution", "revolutions"] # (2 * π) rad
+  unit "tr", value: [1, "cir"], aliases: ["turn", "turns"]
 
-  unit "brad", value: [(Math::PI / 128), "rad"], aliases: ["b°", "bdeg", "binary degree", "binary radian", "binary degrees", "binary radians"] # (π / 128) rad
+  unit "cir", value: [360, "deg"], aliases: ["circle", "circles"]
+  unit "rev", value: [1, "cir"], aliases: ["revolution", "revolutions"]
+  unit "cyc", value: [1, "cir"], aliases: ["cycle", "cycles"]
+  unit "mil", value: [Rational(1, 6400), "cir"], aliases: ["mils"]
+
+  unit "brad", value: [(Math::PI / 128), "rad"], aliases: ["b°", "bdeg", "binary degree", "binary radian", "binary degrees", "binary radians"]
+
+  unit "sign", value: "30 deg", aliases: ["signs"]
+  unit "octant", value: "45 deg", aliases: ["octants"]
+  unit "sextant", value: "60 deg", aliases: ["sextants"]
+  unit "quadrant", value: "90 deg", aliases: ["quadrants"]
 end
