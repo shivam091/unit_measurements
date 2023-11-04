ed89f41c7662f13234d66073d5f9cf9801115df7
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-11-04 18:18:11 +0530

    Added new astronomical and imperial units in `length`

--- lib/unit_measurements/unit_groups/length.rb
+++ lib/unit_measurements/unit_groups/length.rb
@@ -9,6 +9,9 @@ UnitMeasurements::Length = UnitMeasurements.build do
     si_unit "m", aliases: ["meter", "metre", "meters", "metres"]
 
     unit "Å", value: "1e-10 m", aliases: ["angstrom", "angstroms", "ångström"]
+    unit "μ", value: "1 μm", aliases: ["micron", "microns"]
+
+    unit "cb (M)", value: "185.2 m", aliases: ["CBL. (M)", "cable length (M)"]
   end
 
   system :imperial do
@@ -21,21 +24,36 @@ UnitMeasurements::Length = UnitMeasurements.build do
     unit "ch", value: "22 yd", aliases: ["chain", "chains"]
     unit "th", value: "1/1000 in", aliases: ["thou", "thousandth of an inch", "mil", "mils"]
     unit "nl", value: "3 nmi", aliases: ["NL", "nleague", "nleagues", "nautical league", "nautical leagues"]
+    unit "rd", value: "16 1/2 ft", aliases: ["rod", "rods", "perch", "pole", "lug"]
+    unit "cb", value: "608 ft", aliases: ["CBL.", "cable length"]
 
     unit "ftm", value: "6 ft", aliases: ["fathom", "fathoms"]
     unit "fur", value: "220 yd", aliases: ["furlong", "furlongs"]
     unit "nmi", value: "1852 m", aliases: ["NMI", "M", "NM", "nautical mile", "nautical miles"]
-    unit "rod", value: "16 1/2 ft", aliases: ["rods", "perch", "pole", "lug"]
     unit "lnk", value: "33/50 ft", aliases: ["l", "li", "link", "links"]
     unit "lea", value: "3 mi", aliases: ["league", "leagues"]
+
+    unit "b.c.", value: "1/3 in", aliases: ["bc", "barleycorn", "barleycorns"]
   end
 
   system :us_customary do
     unit "sft", value: "1200/3937 m", aliases: ["sfoot", "sfeet", "survey-foot", "survey-feet"]
+
+    unit "cb (US)", value: "720 ft", aliases: ["CBL. (US)", "cable length (US)"]
   end
 
   system :astronomical do
+    unit "S", value: "1e+9 km", aliases: ["spat", "spats"]
+
     unit "au", value: "149597870700 m", aliases: ["astronomical unit", "astronomical units"]
     unit "pc", value: [Rational(648000, Math::PI), "au"], aliases: ["parsec", "parsecs"]
+
+    unit "ls", value: "299792458 m", aliases: ["light-second", "light-seconds"]
+    unit "lmin", value: "60 ls", aliases: ["light-minute", "light-minutes"]
+    unit "lh", value: "60 lmin", aliases: ["light-hour", "light-hours"]
+    unit "ld", value: "24 lh", aliases: ["light-day", "light-days"]
+    unit "lw", value: "7 ld", aliases: ["light-week", "light-weeks"]
+    unit "lm", value: "30 ld", aliases: ["light-month", "light-months"]
+    unit "ly", value: "365.25 ld", aliases: ["light-year", "light-years"]
   end
 end
