cabe302e5329e363885e28516d76080a00337916
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-11-05 19:11:28 +0530

    Deprecated the `#format` method in favour of `#to_fs`

--- lib/unit_measurements/formatter.rb
+++ lib/unit_measurements/formatter.rb
@@ -18,19 +18,27 @@ module UnitMeasurements
     # containing placeholders for +quantity+ and +unit+.
     DEFAULT_FORMAT = "%.2<quantity>f %<unit>s".freeze
 
+    # @deprecated This method has been deprecated in favour of {#to_fs}.
+    #
+    # This method is no longer recommended for use. Please use {#to_fs}
+    # instead.
+    def format(format = nil)
+      warn "DEPRECATION WARNING: The `format` method is deprecated and will be removed in upcoming release. Please use `to_fs` instead."
+      to_fs(format)
+    end
+
     # Formats measurement to certain formatted string specified by +format+.
-    # If +format+ is not specified, it uses +DEFAULT_FORMAT+ for formatting the
+    # If +format+ is not specified, it uses {DEFAULT_FORMAT} for formatting the
     # measurement.
     #
-    # The format method allows for customization of the output format of a
-    # measurement. It uses format placeholders for +quantity+ and +unit+. If no
-    # custom format is provided, it will use the +DEFAULT_FORMAT+.
+    # The {#to_fs} method allows for customization of the output format of a
+    # measurement. It uses format placeholders for +quantity+ and +unit+.
     #
     # @example
-    #   UnitMeasurements::Length.new(1, "m").to("in").format
+    #   UnitMeasurements::Length.new(1, "m").to("in").to_fs
     #   => "39.37 in"
     #
-    #   UnitMeasurements::Length.new(1, "m").to("in").format("%.4<quantity>f %<unit>s")
+    #   UnitMeasurements::Length.new(1, "m").to("in").to_fs("%.4<quantity>f %<unit>s")
     #   => "39.3701 in"
     #
     # @param [String, optional] format
@@ -41,10 +49,9 @@ module UnitMeasurements
     #
     # @see DEFAULT_FORMAT
     # @author {Harshal V. Ladhe}[https://shivam091.github.io/]
-    # @since 1.1.0
-    def format(format = nil)
+    # @since 5.10.0
+    def to_fs(format = nil)
       kwargs = {quantity: quantity, unit: unit.to_s}
-
       (format || DEFAULT_FORMAT) % kwargs
     end
   end
--- spec/unit_measurements/formatter_spec.rb
+++ spec/unit_measurements/formatter_spec.rb
@@ -8,16 +8,26 @@ RSpec.describe UnitMeasurements::Formatter do
   describe "#format" do
     subject { UnitMeasurements::Length.parse("1 m to cm") }
 
+    it "produces a deprecation warning" do
+      expect {
+        subject.format
+      }.to output(a_string_including("DEPRECATION WARNING")).to_stderr
+    end
+  end
+
+  describe "#to_fs" do
+    subject { UnitMeasurements::Length.parse("1 m to cm") }
+
     context "when format is specified" do
       it "formats measurement using specified format" do
-        expect(subject.format("%.1<quantity>f %<unit>s")).to eq("100.0 cm")
-        expect(subject.format("%.1<quantity>f")).to eq("100.0")
+        expect(subject.to_fs("%.1<quantity>f %<unit>s")).to eq("100.0 cm")
+        expect(subject.to_fs("%.1<quantity>f")).to eq("100.0")
       end
     end
 
     context "when format is not specified" do
       it "formats measurement using default format" do
-        expect(subject.format).to eq("100.00 cm")
+        expect(subject.to_fs).to eq("100.00 cm")
       end
     end
   end
