09582e89ef25d2faffebb1239962adcc7af09f3b
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-11-29 08:15:57 +0530

    sd

--- lib/unit_measurements/math.rb
+++ lib/unit_measurements/math.rb
@@ -166,5 +166,137 @@ module UnitMeasurements
     def log2
       self.class.new(::Math.log2(quantity), unit)
     end
+
+    # Returns a new +Measurement+ instance with the sine of the quantity.
+    #
+    # @return [Measurement] A new +Measurement+ instance with the sine.
+    # @example
+    #   measurement = UnitMeasurements::Angle.new(90, "deg")
+    #   result = measurement.sin
+    #   #=> 1.0
+    def sin
+      self.class.new(Math.sin(quantity), unit)
+    end
+
+    # Returns a new +Measurement+ instance with the arcsine of the quantity.
+    #
+    # @return [Measurement] A new +Measurement+ instance with the arcsine.
+    # @example
+    #   measurement = UnitMeasurements::Angle.new(1, "")
+    #   result = measurement.asin
+    #   #=> 90.0 deg
+    def asin
+      self.class.new(Math.asin(quantity), unit)
+    end
+
+    # Returns a new +Measurement+ instance with the cosine of the quantity.
+    #
+    # @return [Measurement] A new +Measurement+ instance with the cosine.
+    # @example
+    #   measurement = UnitMeasurements::Angle.new(60, "deg")
+    #   result = measurement.cos
+    #   #=> 0.5
+    def cos
+      self.class.new(Math.cos(quantity), unit)
+    end
+
+    # Returns a new +Measurement+ instance with the arccosine of the quantity.
+    #
+    # @return [Measurement] A new +Measurement+ instance with the arccosine.
+    # @example
+    #   measurement = UnitMeasurements::Angle.new(0.5, "")
+    #   result = measurement.acos
+    #   #=> 60.0 deg
+    def acos
+      self.class.new(Math.acos(quantity), unit)
+    end
+
+    # Returns a new +Measurement+ instance with the hyperbolic sine of the quantity.
+    #
+    # @return [Measurement] A new +Measurement+ instance with the hyperbolic sine.
+    # @example
+    #   measurement = UnitMeasurements::Length.new(1, "m")
+    #   result = measurement.sinh
+    #   #=> 1.1752011936438 m
+    def sinh
+      self.class.new(Math.sinh(quantity), unit)
+    end
+
+    # Returns a new +Measurement+ instance with the hyperbolic cosine of the quantity.
+    #
+    # @return [Measurement] A new +Measurement+ instance with the hyperbolic cosine.
+    # @example
+    #   measurement = UnitMeasurements::Length.new(1, "m")
+    #   result = measurement.cosh
+    #   #=> 1.5430806348152 m
+    def cosh
+      self.class.new(Math.cosh(quantity), unit)
+    end
+
+    # Returns a new +Measurement+ instance with the tangent of the quantity.
+    #
+    # @return [Measurement] A new +Measurement+ instance with the tangent.
+    # @example
+    #   measurement = UnitMeasurements::Angle.new(45, "deg")
+    #   result = measurement.tan
+    #   #=> 1.0
+    def tan
+      self.class.new(Math.tan(quantity), unit)
+    end
+
+    # Returns a new +Measurement+ instance with the hyperbolic tangent of the quantity.
+    #
+    # @return [Measurement] A new +Measurement+ instance with the hyperbolic tangent.
+    # @example
+    #   measurement = UnitMeasurements::Angle.new(1, "")
+    #   result = measurement.tanh
+    #   #=> 0.76159415595576
+    def tanh
+      self.class.new(Math.tanh(quantity), unit)
+    end
+
+    # Returns a new +Measurement+ instance representing the hypotenuse of two measurements.
+    #
+    # @param [Measurement] other The other measurement to form a right-angled triangle.
+    # @return [Measurement] A new +Measurement+ instance with the hypotenuse.
+    # @raise [ArgumentError] If the units of the measurements are different.
+    # @example
+    #   measurement1 = UnitMeasurements::Length.new(3, "m")
+    #   measurement2 = UnitMeasurements::Length.new(4, "m")
+    #   result = measurement1.hypot(measurement2)
+    #   #=> 5.0 m
+    def hypot(other)
+      raise ArgumentError, "Both measurements must have the same unit" unless unit == other.unit
+
+      self.class.new(Math.hypot(quantity, other.quantity), unit)
+    end
+
+    # Returns a new +Measurement+ instance with the arctangent of the quantity.
+    #
+    # @return [Measurement] A new +Measurement+ instance with the arctangent.
+    # @example
+    #   measurement = UnitMeasurements::Angle.new(1, "")
+    #   result = measurement.atan
+    #   #=> 45.0 deg
+    def atan
+      self.class.new(Math.atan(quantity), unit)
+    end
+
+    # Returns a new +Measurement+ instance with the arctangent given two measurements.
+    #
+    # @param [Measurement] other The other measurement for the atan2 operation.
+    # @return [Measurement] A new +Measurement+ instance with the arctangent.
+    # @raise [ArgumentError] If the units of the measurements are different.
+    # @example
+    #   x = UnitMeasurements::Length.new(3, "m")
+    #   y = UnitMeasurements::Length.new(4, "m")
+    #   result = x.atan2(y)
+    #   #=> 36.86989764584402 deg
+    def atan2(other)
+      raise ArgumentError, "Both measurements must have the same unit" unless unit == other.unit
+
+      self.class.new(Math.atan2(quantity, other.quantity), unit)
+    end
+
   end
 end
